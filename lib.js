fde3f683192d18094ce0c800266ba6d66dcdf136
wallacethefmh <wills.boyd@gmail.com>
2015-03-08 22:38:29 -0400

    progress
    need to figure out a good way to do effects.

--- lib.js
+++ lib.js
@@ -8,10 +8,19 @@ cyclone.helpers = {
 		return new THREE.Vector3(0,-10,0);
 	},
 	addScaledVector: function(vector1,vector2,s) {
+		// console.log(vector1);
+		// console.log('plus');
+		// console.log(vector2);
+		// console.log('scaled by');
+		// console.log(s);
+		// console.log('equals');
+		// console.log(vector1.x + vector2.x * s);
+		// console.log(vector1.y + vector2.y * s);
+		// console.log(vector1.z + vector2.z * s);
 		return new THREE.Vector3(
-				vector1.x + vector2.x*s,
-				vector1.y + vector2.y*s,
-				vector1.z + vector2.z*s
+				vector1.x + vector2.x * s,
+				vector1.y + vector2.y * s,
+				vector1.z + vector2.z * s
 			);
 	}
 }
@@ -20,29 +29,182 @@ cyclone.particle = function(mesh) {
 
 	this.mesh = {};
 
-	this.init = function(mesh) {
+	this.init = function(mesh) 
+	{
 		this.mesh = mesh;
 		this.mesh.velocity = new THREE.Vector3(0,0,0);
 		this.mesh.acceleration = cyclone.helpers.getGravity();
+		this.mesh.damping = .99;
+		this.stillExists = true;
+		this.emitter = false;
 	}
 	this.init(mesh);
 
 	// t should be the time since the last frame.
-	this.update = function(t) {
-
+	this.update = function(t)  
+	{
 		if (!(t > 0)) return false;
 
 		var position = cyclone.helpers.addScaledVector(this.mesh.position, this.mesh.velocity, t);
 		this.mesh.position.set(position.x, position.y, position.z);
-		
+
 		var velocity = cyclone.helpers.addScaledVector(this.mesh.velocity, this.mesh.acceleration, t);;
 		this.mesh.velocity.set(velocity.x, velocity.y, velocity.z);
 		
-		count += 1;
-		
+		count += 1;		
+
+		if (this.mesh.position.y < 0) {
+			scene.remove(this.mesh);
+			this.stillExists = false;
+		}
+
+		if (this.emitter) {
+			this.emitter.emit(t, this.mesh.velocity, this.mesh.position);
+		}
 	}
 
-	this.print = function() {
+	this.setPosition = function(x,y,z) {
+		this.mesh.position.set(x,y,z);
+	}
+
+	this.setMass = function(mass) 
+	{
+	    if (mass == 0) return false;
+	    this.mesh.inverseMass = (1.0)/mass;
+	}
+
+	this.getMass = function() 
+	{
+	    if (this.mesh.inverseMass == 0) {
+	        return Number.POSITIVE_INFINITY;
+	    } else {
+	        return (1.0)/this.mesh.inverseMass;
+	    }
+	}
+
+	this.setInverseMass = function(inverseMass)
+	{
+	   this.mesh.inverseMass = inverseMass;
+	}
+
+	this.getInverseMass = function()
+	{
+	    return this.mesh.inverseMass;
+	}
+
+	this.hasFiniteMass = function()
+	{
+	    return this.mesh.inverseMass >= 0.0;
+	}
+
+	this.setDamping = function(damping)
+	{
+	    this.mesh.damping = damping;
+	}
+
+	this.getDamping = function()
+	{
+	    return this.mesh.damping;
+	}
+
+	this.setVelocity = function(velocity)
+	{
+	    this.mesh.velocity = velocity;
+	}
+
+	this.getVelocity = function()
+	{
+	    this.mesh.velocity;
+	}
+
+	this.setAcceleration = function(acceleration)
+	{
+	    this.mesh.acceleration = acceleration;
+	}
+
+	this.getAcceleration = function()
+	{
+	    return this.mesh.acceleration;
+	}
+
+	this.clearAccumulator = function()
+	{
+	    this.mesh.forceAccum.clear();
+	}
+
+	this.addForce = function(force)
+	{
+	    this.mesh.forceAccum += force;
+	}
+
+	this.setEmitter = function(n, duration)
+	{
+		this.emitter = new emitter(n, duration);
+	}
+}
+
+var emitter = function(n, duration) {
+	
+	this.particles = [];
+	this.count = n;
+	this.started = false;
+	this.finished = false;
+	this.pool = [];
+
+	for (var i = 1000 - 1; i >= 0; i--) {
+		var geometry = new THREE.BoxGeometry( 2, 2, 2 );
+		var material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
+		var mesh = new THREE.Mesh( geometry, material );
+
+		//spark.mesh.castShadow = true;
+		this.pool.push(mesh);
+	};
+
+	console.log(this.pool);
+
+	// this.duration = duration;
+
+	this.emit = function(msElapsed, velocity, position) {
+
+		var indexesToRemove = [];
+		for (var i = this.particles.length - 1; i >= 0; i--) {
+			if (!this.started) this.started = true;
+			if (!this.particles[i].stillExists) indexesToRemove.push(i);
+			this.particles[i].update(msElapsed);
+		};
+		for (var i = indexesToRemove.length - 1; i >= 0; i--) {
+			// return the particle to the available pool
+			this.pool.push(this.particles[indexesToRemove[i]].mesh);
+			this.particles.splice(indexesToRemove[i], 1);
+		};
+
+		for (var i = this.count - 1; i >= 0; i--) {
+			
+			var newSpark = this.pool.pop();
+
+			if (newSpark !== 'undefined') {
+
+				var sparkVelocity = {
+					x : velocity.x + (Math.floor(Math.random() * 400) - 200),
+					y : velocity.y + (Math.floor(Math.random() * 400) - 200),
+					z : velocity.z + (Math.floor(Math.random() * 400) - 200),
+				}
+
+				var spark = new cyclone.particle(newSpark);
+				//spark.setMass(.01); // 1.0kg - mostly blast damage
+				spark.setVelocity(new THREE.Vector3(sparkVelocity.x, sparkVelocity.y, sparkVelocity.z)); //
+				spark.setAcceleration(new THREE.Vector3(0, -250.0, 0));
+				spark.setDamping(0.9);
+				spark.setPosition(position.x, position.y, position.z);
+
+				this.particles.push(spark);
+				scene.add(spark.mesh);
+			} else {
+				console.log('spark array is empty');
+			}
+
+			
+		};
 		
 	}
 
--- main.js
+++ main.js
@@ -1,43 +1,80 @@
 
-var scene = new THREE.Scene(),
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 1000 );
-camera.position.set( 60, 50, 60 );
+var particles = [],
+	stopRender = false,
+	scene = new THREE.Scene(),
+	camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1,10000 );
+
+camera.position.set( 1500, 2400, 1500 );
+
 camera.lookAt( scene.position );
 
+// Renderer
+
 var renderer = new THREE.WebGLRenderer();
+renderer.shadowMapEnabled = true;
+renderer.shadowMapSoft = true;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.PlaneGeometry( 75, 75, 50, 50 );
-var material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-var plane = new THREE.Mesh( geometry, material );
-plane.rotation.x = Math.PI / -2;
-scene.add( plane )
+// Ground
 
-var particles = [];
+var groundGeo = new THREE.PlaneBufferGeometry( 5000, 5000 );
+var groundMat = new THREE.MeshPhongMaterial( { ambient: 0xffffff, color: 0x89B3E0, specular: 0x05FF1A } );
+//groundMat.color.setHSL( 0.095, 1, 0.75 );
 
-var geometry1 = new THREE.BoxGeometry( 3, 3, 1 );
-var material1 = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-var cube1 = new THREE.Mesh( geometry1, material1 );
-cube1.position.x = 5;
-cube1.position.y = 21;
+var ground = new THREE.Mesh( groundGeo, groundMat );
+ground.rotation.x = -Math.PI/2;
+ground.position.y = -33;
 
-var geometry2 = new THREE.BoxGeometry( 3, 5, 4 );
-var material2 = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-var cube2 = new THREE.Mesh( geometry2, material2 );
-cube2.position.x = -5;
-cube2.position.y = 25;
+scene.add(ground);
 
-particles.push(new cyclone.particle(cube1));
-particles.push(new cyclone.particle(cube2));
+ground.receiveShadow = true;
 
-for (var i = particles.length - 1; i >= 0; i--) {
-	scene.add( particles[i].mesh );
-};
+// // Lights
+
+// hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+// hemiLight.color.setHSL( 0.6, 1, 0.6 );
+// hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+// hemiLight.position.set( 0, 500, 0 );
+// scene.add( hemiLight );
+
+light = new THREE.DirectionalLight(0xdfebff, .75);
+light.position.set(0, 400, 0);
+light.position.multiplyScalar(1.3);
+
+light.castShadow = true;
+// light.shadowCameraVisible = true;
+
+light.shadowMapWidth = 2048;
+light.shadowMapHeight = 2048;
+
+var d = 2500;
+
+light.shadowCameraLeft = -d;
+light.shadowCameraRight = d;
+light.shadowCameraTop = d;
+light.shadowCameraBottom = -d;
+
+light.shadowCameraFar = 1000;
+light.shadowDarkness = 0.5;
+light.shadowBias = -0.0001;
+scene.add(light);
+
+// mouse movement to camera
+var mouseX = 0;
+var mouseY = 0;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
 var time = -1;
-
 var render = function () {
+
+	if (stopRender) {
+		return false;
+	}
+
+	camera.lookAt(ground.position);
+
 	requestAnimationFrame( render );
 
 	if (time === -1) {
@@ -48,12 +85,72 @@ var render = function () {
 	}
 	time = new Date();
 
+	var indexesToRemove = [];
 	for (var i = particles.length - 1; i >= 0; i--) {
+		if (!particles[i].stillExists) indexesToRemove.push(i);
 		particles[i].update(difference/1000);
 	};
+	for (var i = indexesToRemove.length - 1; i >= 0; i--) {
+		particles.splice(indexesToRemove[i], 1);
+	};
 
-	
 	renderer.render(scene, camera);
+
 };
 
+var mouse = {};
+var raycaster = new THREE.Raycaster();
+var velocityFactor = 650;
+
+// click to create fireball
+document.addEventListener('mousedown', shoot, false);
+function shoot(event) {
+	event.preventDefault();
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+	raycaster.setFromCamera( mouse, camera );	
+
+	// calculate objects intersecting the picking ray
+	var intersects = raycaster.intersectObject( ground );
+	if (intersects.length) {
+		var norm = intersects[0].point.normalize(),
+			velocityX = norm.x * velocityFactor,
+			velocityZ = norm.z * velocityFactor;
+
+		var geometry = new THREE.SphereGeometry( 22, 12, 12 );
+		var material = new THREE.MeshPhongMaterial({
+			ambient: 0x030303, 
+			color: 0xED6F15, 
+			specular: 0xED6F15, 
+			shininess: 4, 
+			shading: THREE.FlatShading 
+		});
+		var mesh = new THREE.Mesh( geometry, material );
+		
+		var fireball = new cyclone.particle(mesh);
+		fireball.setMass(1.0); // 1.0kg - mostly blast damage
+		fireball.setVelocity(new THREE.Vector3(velocityX,60,velocityZ)); //
+		fireball.setAcceleration(new THREE.Vector3(0, -65.0, 0));
+		fireball.setDamping(0.9);
+		fireball.setPosition(0,80,0);
+		fireball.mesh.castShadow = true;
+		fireball.setEmitter(20,150);
+
+		particles.push(fireball);
+		scene.add(fireball.mesh);
+	}
+	
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+function keyDownHandler(event) {
+	if (event.keyCode == 81) {
+		stopRender = true;
+	} else {
+		console.log('hit key: ' + event.keyCode);
+	}
+}
+
+// start the ap
 render();
